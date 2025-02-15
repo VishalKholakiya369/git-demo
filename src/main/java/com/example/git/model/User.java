a715721f89a2446e91f4050e122adb2377853711
VishalKholakiya369 <vishalkholakiya369@gmail.com>
2025-02-15 16:11:01 +0530

    Modified User -Email fild is adding

--- src/main/java/com/example/git/model/User.java
+++ src/main/java/com/example/git/model/User.java
@@ -3,7 +3,16 @@ package com.example.git.model;
 public class User {
     private int id;
     private String userName;
+    private String email;
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getPassword() {
         return password;
